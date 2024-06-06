f8b79159c0739e085e76e511cce1749f6b0a0b3e
Wnagfeng <2712442320@qq.com>
2024-06-07 03:58:43 +0800

    推送项目

---
+++ url.ts
@@ -0,0 +1,15 @@
+import { URL } from 'url';
+/**
+ * @Author: Joon
+ * @Date: 2021-07-16 16:47:27
+ * @LastEditors: Joon
+ * @Description: 根据key从一段url中获取query值
+ * @param urlPath {String} url地址
+ * @param key {String} 获取单独的一个key
+ * @return {*}
+ */
+export const getUrlQuery = (urlPath: string, key: string): string | null => {
+  const url = new URL(urlPath, 'https://www.');
+  const params = new URLSearchParams(url.search.substring(1));
+  return params.get(key);
+};
